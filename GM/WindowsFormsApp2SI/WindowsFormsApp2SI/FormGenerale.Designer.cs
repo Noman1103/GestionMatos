7bb9c70820b962c087981cae1b5558fd98e711db
Noman1103 <130471196+Noman1103@users.noreply.github.com>
2023-04-17 19:34:16 +0200

    Add files via upload

---
+++ GM/WindowsFormsApp2SI/WindowsFormsApp2SI/FormGenerale.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace WindowsFormsApp2SI
+{
+    partial class Form2SI
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.parametresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.matérielsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonCI = new System.Windows.Forms.Button();
+            this.comboBoxMatos = new System.Windows.Forms.ComboBox();
+            this.textBoxTech = new System.Windows.Forms.TextBox();
+            this.textBoxComment = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePickerDI = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.parametresToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // parametresToolStripMenuItem
+            // 
+            this.parametresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clientsToolStripMenuItem,
+            this.matérielsToolStripMenuItem});
+            this.parametresToolStripMenuItem.Name = "parametresToolStripMenuItem";
+            this.parametresToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.parametresToolStripMenuItem.Text = "Parametres";
+            // 
+            // clientsToolStripMenuItem
+            // 
+            this.clientsToolStripMenuItem.Name = "clientsToolStripMenuItem";
+            this.clientsToolStripMenuItem.Size = new System.Drawing.Size(153, 26);
+            this.clientsToolStripMenuItem.Text = "Clients";
+            this.clientsToolStripMenuItem.Click += new System.EventHandler(this.clientsToolStripMenuItem_Click);
+            // 
+            // matérielsToolStripMenuItem
+            // 
+            this.matérielsToolStripMenuItem.Name = "matérielsToolStripMenuItem";
+            this.matérielsToolStripMenuItem.Size = new System.Drawing.Size(153, 26);
+            this.matérielsToolStripMenuItem.Text = "Matériels";
+            this.matérielsToolStripMenuItem.Click += new System.EventHandler(this.matérielsToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonCI);
+            this.groupBox1.Controls.Add(this.comboBoxMatos);
+            this.groupBox1.Controls.Add(this.textBoxTech);
+            this.groupBox1.Controls.Add(this.textBoxComment);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dateTimePickerDI);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 273);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Création d\'une intervention";
+            // 
+            // buttonCI
+            // 
+            this.buttonCI.Location = new System.Drawing.Point(22, 220);
+            this.buttonCI.Name = "buttonCI";
+            this.buttonCI.Size = new System.Drawing.Size(303, 41);
+            this.buttonCI.TabIndex = 8;
+            this.buttonCI.Text = "Créer";
+            this.buttonCI.UseVisualStyleBackColor = true;
+            this.buttonCI.Click += new System.EventHandler(this.buttonCI_Click);
+            // 
+            // comboBoxMatos
+            // 
+            this.comboBoxMatos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMatos.FormattingEnabled = true;
+            this.comboBoxMatos.Location = new System.Drawing.Point(125, 190);
+            this.comboBoxMatos.Name = "comboBoxMatos";
+            this.comboBoxMatos.Size = new System.Drawing.Size(200, 24);
+            this.comboBoxMatos.TabIndex = 7;
+            // 
+            // textBoxTech
+            // 
+            this.textBoxTech.Location = new System.Drawing.Point(125, 139);
+            this.textBoxTech.Name = "textBoxTech";
+            this.textBoxTech.Size = new System.Drawing.Size(200, 22);
+            this.textBoxTech.TabIndex = 6;
+            // 
+            // textBoxComment
+            // 
+            this.textBoxComment.Location = new System.Drawing.Point(125, 78);
+            this.textBoxComment.Multiline = true;
+            this.textBoxComment.Name = "textBoxComment";
+            this.textBoxComment.Size = new System.Drawing.Size(200, 50);
+            this.textBoxComment.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Matériel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Technicien";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Commentaire";
+            // 
+            // dateTimePickerDI
+            // 
+            this.dateTimePickerDI.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerDI.Location = new System.Drawing.Point(125, 36);
+            this.dateTimePickerDI.Name = "dateTimePickerDI";
+            this.dateTimePickerDI.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerDI.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Date";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form2SI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2SI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestion Matos";
+            this.Load += new System.EventHandler(this.Form2SI_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem parametresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem matérielsToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxTech;
+        private System.Windows.Forms.TextBox textBoxComment;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDI;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonCI;
+        private System.Windows.Forms.ComboBox comboBoxMatos;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ GM/WindowsFormsApp2SI/WindowsFormsApp2SI/FormGenerale.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsGMF;
+
+namespace WindowsFormsApp2SI
+{
+    public partial class Form2SI : Form
+    {
+        private string strcon = @"Server = .\SQLEXPRESS; Database=GM;Trusted_Connection=True;";
+
+        public Form2SI()
+        {
+            InitializeComponent();
+        }
+
+        private void clientsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormClients dlg = new FormClients();
+            dlg.ShowDialog();
+        }
+
+        private void matérielsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormMateriel dlg = new FormMateriel();
+            dlg.ShowDialog();
+        }
+
+        private void Form2SI_Load(object sender, EventArgs e)
+        {
+           FormConnect dlg = new FormConnect();
+           dlg.ShowDialog();
+
+
+           fillComboMatos();
+        }
+
+        private void fillComboMatos()
+        {
+            comboBoxMatos.Items.Clear();
+            SqlConnection cn = new SqlConnection(strcon);
+            cn.Open();
+            string sql = "select * from MATERIEL";
+            SqlCommand com = new SqlCommand(sql, cn);
+            SqlDataReader dr = com.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBoxMatos.Items.Add(dr["Nom"].ToString());
+            }
+
+            dr.Close();
+            cn.Close();
+        }
+
+        private int getMatosID(string name)
+        {
+            SqlConnection cn = new SqlConnection(strcon);
+            cn.Open();
+            
+            string strsqL = "select ID_MAT from MATERIEL where Nom = '" + name + "'";
+
+            SqlCommand com = new SqlCommand(strsqL, cn);
+            SqlDataReader dr = com.ExecuteReader();
+            dr.Read();
+            int nb = Convert.ToInt32(dr["ID_MAT"]);
+            cn.Close();
+
+            return nb;
+        }
+
+
+        private void buttonCI_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(strcon);
+            cn.Open();
+
+            string Commentaire = textBoxComment.Text;
+            string tech = textBoxTech.Text;
+            string dateInstall = dateTimePickerDI.Value.ToString("yyyy-MM-dd");
+            int idc = getMatosID(comboBoxMatos.SelectedItem.ToString());
+                        
+            string strsqL = "insert into Intervention values('" + dateInstall + "','" +
+                Commentaire + "','" + tech + "'," + idc + ")";
+
+            SqlCommand com = new SqlCommand(strsqL, cn);
+            com.ExecuteNonQuery();
+            cn.Close();
+
+            cleanInter();
+
+            MessageBox.Show("Intervention bien ajoutée !", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+        }
+
+        private void cleanInter()
+        {
+            textBoxTech.Text = textBoxComment.Text = "";
+
+            dateTimePickerDI.Value = DateTime.Today;
+            comboBoxMatos.SelectedIndex = -1;
+        }
+    }
+}
